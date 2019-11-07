7ce0af89670cccc7051a2dc193d9df65cce93342
joker <joker@tjo.biz>
2019-11-08 00:02:42 +0700

    First version Materialize Form HTML5 The object FormData without jQuery ES6 - JavaScript
     Structure:
    HTML landing page - FormData -> AJAX -> JSON (or FormData) -> PHP Script(Can use node.js for JSON Array) -> sent message to mail, SMS service -> response JSON (or FormData) to Landing Page or sites
    
    Change links to YOU Site in HTML document!!!

--- jsonmailformjsajax.php
+++ jsonmailformjsajax.php
@@ -2,8 +2,8 @@
 // This is script MaterializeCSS use 3 PHP products. You must integration it if you need those functional
 // 1) PHPMailer do email message from forms - https://github.com/PHPMailer/PHPMailer
 // 2) SMS.RU do mobile sms message from forms too you phone number - https://sms.ru (You need this account)
-// 3) Telegram privet room (High security channel) for message to your mobile and employers mobiles your company. - https://github.com/tg-bot-api/bot-api-base
-// And this is my backend PHP script. My e-mail is joker@tjo.biz. Support only of money.
+// 3) Telegram darknet rooms (High security channel) for message to your mobile and staf mobiles your company. - https://github.com/tg-bot-api/bot-api-base
+// And this is my backend PHP script. My e-mail is joker@tjo.biz. Support "No money, no honey".
 // Handling data in JSON format on the server-side using PHP
 header("Content-Type: application/json");
 // build a PHP variable from JSON sent using POST method
